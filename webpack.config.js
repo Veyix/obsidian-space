f73ad20e603b8d3edbdcc1c493743051fdcd0d58
Samuel Slade <samuel.pjm.slade@googlemail.com>
2020-04-23 22:23:07 +0100

    Setup webpack dev server to enable hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,13 @@ const path = require("path")
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "public/dist"),
     filename: "app.js"
   },
-  watch: true
+  watch: true,
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 9000,
+    publicPath: "/dist/"
+  }
 }
